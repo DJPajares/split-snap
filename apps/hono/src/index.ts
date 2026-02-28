c5d11e21ddc4c68b11f88e28ee825f96b2413c14
DJPajares <dj.pajares@gmail.com>
2026-02-28 22:52:26 +0800

    test: fix vercel

--- apps/hono/src/index.ts
+++ apps/hono/src/index.ts
@@ -1,14 +1,2 @@
-import { Hono } from 'hono'
-
-const app = new Hono()
-
-const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
-
-app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
-
-export default app
+import app from './app.js';
+export default app;
